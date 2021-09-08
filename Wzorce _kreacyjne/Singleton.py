ebf692f5118122855fb55da385304d0d31dc431f
Lukasz P <lukasz1990@onet.pl>
2021-09-08 20:21:57 +0200

    potwierdzenie singeltona

--- Wzorce _kreacyjne/Singleton.py
+++ Wzorce _kreacyjne/Singleton.py
@@ -16,20 +16,44 @@ class Singleton_Genius(object):
         if plik.writable():
             plik.write(self.text)
             plik.write("\n")
-        plik.close()
+            plik.close()
+        else:
+            print("Problem z otwarciem pliku")
 
-s1 = Singleton_Genius("Yang", "Zhou","s1")
-s2 = Singleton_Genius("Elon", "Musk","s2")
+s1 = Singleton_Genius("Helm", "Gamma","string1")
+s2 = Singleton_Genius("Johnson", "Vlissides", "string2")
 s1.plik_tekstowy()
 s2.plik_tekstowy()
 
-print(s1==s2)
-a = 4
-b = a
-print("v1", b == a)
-a = 5
-print("v2", b == a)
+print("Czy s1 i s2 to ten sam obiekt: ", s1==s2)
 
+object_id = id(s1)
+hexadecimal = hex(object_id)
+print(hexadecimal)
 
+object_id = id(s2)
+hexadecimal = hex(object_id)
+print(hexadecimal)
 
+class testowa:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
 
+    def przedstawiam_sie(self):
+        return f"nazywam sie:" + self.imie + " " + self.nazwisko
+
+print("")
+test = testowa("Lukasz", "Prządka")
+print(test.przedstawiam_sie())
+test1 = testowa("Lukasz", "Prządka")
+print(test.przedstawiam_sie())
+print("Czy test i test1 to ten sam obiekt: ", test == test1)
+
+object_id = id(test)
+hexadecimal = hex(object_id)
+print(hexadecimal)
+
+object_id = id(test1)
+hexadecimal = hex(object_id)
+print(hexadecimal)
